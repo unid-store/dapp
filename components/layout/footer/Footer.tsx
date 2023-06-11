2356e2ef5071f9eeed1ca05f3d63433a07deccf1
Pylyp Radionov <mr1346@protonmail.com>
2023-06-11 14:57:39 +0700

    revert footer.

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -6,26 +6,14 @@ import { FaGithub } from "react-icons/fa";
 export const Footer = () => {
   return (
     <footer className="bg-gradient-to-r from-gray-700 to-gray-900 text-white py-2 px-2">
-      <div className="flex justify-between items-center">
-        <div className="flex items-center">
-          <a
-            href="https://github.com/unid-store"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mr-4"
-          >
-            <FaGithub size={24} />
-          </a>
-        </div>
-        <div className="flex items-center">
-          <Link href="/tc">
-            <TermsAndConditionsIcon />
-          </Link>
-
-          <Link href="/pp" className="ml-4">
-            <PrivacyPolicyIcon />
-          </Link>
-        </div>
+      <div className="flex justify-center">
+        <a
+          href="https://github.com/unid-store"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub size={24} />
+        </a>
       </div>
     </footer>
   );
