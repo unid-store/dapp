184eff7f0f17a6ecc6fa826a2b8250b1638383dd
Pylyp Radionov <mr1346@protonmail.com>
2023-06-15 03:23:58 +0700

    remove `Close` btn in toast.

--- components/ui/Toast.tsx
+++ components/ui/Toast.tsx
@@ -32,9 +32,6 @@ export const Toast = ({ onClose, title, subtitle, timeout }: ToastProps) => {
       <div className="bg-white rounded-lg p-4 shadow-lg border border-gray-200 text-black flex flex-col items-start">
         <h2 className="text-2xl font-bold mb-2">{title}</h2>
         {subtitle && <p className="text-gray-600">{subtitle}</p>}
-        <Button variant={"default"} onClick={onClose} className="mt-2 ml-auto">
-          {"Close"}
-        </Button>
       </div>
     </motion.div>
   );
