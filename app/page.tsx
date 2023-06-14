fa3cc5cf39bc1718d8ba1e9d85c57a2893927580
Pylyp Radionov <mr1346@protonmail.com>
2023-06-15 04:54:14 +0700

    add vercel analytics.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import Upload from "@/components/upload/Upload";
 
-const Home = () => <Upload />;
+const Home = () => (
+  <>
+    <Upload />;
+    <Analytics />
+  </>
+);
+
 export default Home;
