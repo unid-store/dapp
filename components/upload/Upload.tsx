eed3dd97e6ee2fac0a9231b71362c358f28017e2
Pylyp Radionov <mr1346@protonmail.com>
2023-06-15 03:59:18 +0700

    simplify Upload.tsx.

--- components/upload/Upload.tsx
+++ components/upload/Upload.tsx
@@ -11,16 +11,15 @@ import { upload } from "./HandleUpload";
 import FileTable from "./FileTable";
 
 export default function Upload() {
+  const [uploading, setUploading] = useState<boolean>();
   const [files, setFiles] = useState<File[]>([]);
+  const [exists, setExists] = useState<boolean>();
   const [cid, setCID] = useState<string>();
-  const [exists, setExists] = useState(false);
 
-  const [uploading, setUploading] = useState<boolean>(false);
-
-  const handleUpload = async (files: File[]) => {
-    setFiles(files);
+  const handleUpload = async (acceptedFiles: File[]) => {
     setUploading(true);
-    const { cid, exists } = await upload(files);
+    setFiles(acceptedFiles);
+    const { cid, exists } = await upload(acceptedFiles);
     setCID(cid);
     setExists(exists);
     setUploading(false);
@@ -31,31 +30,26 @@ export default function Upload() {
     setFiles([]);
   };
 
+  const uploadState = <FileDropZone onDrop={handleUpload} />;
+
+  const resultState = (
+    <div className="flex flex-col">
+      <FileTable files={files} cid={cid!} />
+      <Button onClick={handleReset} className="mt-4 w-full text-center">
+        {"Upload Again"}
+      </Button>
+    </div>
+  );
+
   return (
     <>
-      {uploading ? (
-        <Spinner />
-      ) : cid ? (
-        <div>
-          {exists && (
-            // @TODO refactor `WithToast` and `Toast` to allow show conditionally toast, or conditional wrapper
-            <WithToast
-              title={`✅ uploaded already `}
-              subtitle={"check the link!"}
-              show
-            >
-              <></>
-            </WithToast>
-          )}
-          <FileTable files={files} cid={cid} />
-          <Button onClick={handleReset} className="mt-4 w-full text-center">
-            {"Upload Again"}
-          </Button>
-        </div>
-      ) : (
-        <>
-          <FileDropZone onDrop={async (files: File[]) => handleUpload(files)} />
-        </>
+      {(uploading && <Spinner />) || (!cid && uploadState) || resultState}
+      {exists && (
+        <WithToast
+          title={`✅ uploaded already `}
+          subtitle={"check the link!"}
+          show
+        />
       )}
     </>
   );
